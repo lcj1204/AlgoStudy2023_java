ee705303ac643b7fa1de5631b9c72ee6cae2b234
lcj1204 <dlckdwo1204@naver.com>
2024-03-06 16:15:01 +0900

    일곱 난쟁이 - 구현

--- src/main/java/week1/boj_2309.java
+++ src/main/java/week1/boj_2309.java
@@ -3,30 +3,41 @@ package week1;
 import java.util.*;
 
 public class boj_2309 {
+    static ArrayList<Integer> arr = new ArrayList<>();
+    static int target = 0; // 두개를 더해서 target이 되는 조합을 찾는다.
     public static void main(String[] args) {
-        boj_2309 boj2309 = new boj_2309();
-        boj2309.solution();
-    }
-
-    int[] arr = new int[9];
-    int[] answer = new int[7];
-    int cnt = 0;
-
-    public void solution() {
         Scanner scanner = new Scanner(System.in);
-
         for (int i = 0; i < 9; i++){
-            arr[i] = scanner.nextInt();
+            arr.add(scanner.nextInt());
         }
 
-        back(arr, answer);
-    }
+        target = arr.stream().
+                mapToInt(Integer::intValue)
+                .sum() - 100;
 
-    public void back(int[] arr, int[] answer) {
-        if (answer.length == 7) {
-            if (Arrays.stream(arr).sum() == 100) {
+        int find = 0;
+        boolean flag = false;
+        for (int i = 0; i < arr.size() - 1; i++) {
 
+            find = target - arr.get(i);
+
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr.get(j) == find) {
+                    arr.remove(j);
+                    flag = true;
+                    break;
+                }
             }
+
+            if (flag) {
+                arr.remove(i);
+                Collections.sort(arr);
+
+                arr.forEach(System.out::println);
+                return;
+            }
+
         }
+
     }
 }
